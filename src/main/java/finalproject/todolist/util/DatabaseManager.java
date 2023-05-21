3a41fc872b169cd72a8889cb01e0a95bf6dcdfcd
Lefia <lefia1222@gmail.com>
2023-05-22 02:26:41 +0800

    feat: 新增任務到資料庫、回傳資料庫的資料

--- src/main/java/finalproject/todolist/util/DatabaseManager.java
+++ src/main/java/finalproject/todolist/util/DatabaseManager.java
@@ -1,6 +1,8 @@
 package finalproject.todolist.util;
 
+import finalproject.todolist.component.Task;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DatabaseManager {
     private String filePath;
@@ -19,7 +21,7 @@ public class DatabaseManager {
         return filePath;
     }
 
-    public void connect() {
+    private void connect() {
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:" + filePath);
         } catch (SQLException e) {
@@ -27,7 +29,7 @@ public class DatabaseManager {
         }
     }
 
-    public void disconnect() {
+    private void disconnect() {
         try {
             connection.close();
         } catch (SQLException e) {
@@ -35,9 +37,33 @@ public class DatabaseManager {
         }
     }
 
+    public void addTask(String taskName, String taskDescription, String date) throws SQLException {
+        connect();
+        Statement statement = connection.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS Inbox (Name string, Description string, Date string)");
+        statement.execute("INSERT INTO Inbox VALUES ('%s', '%s', '%s')".formatted(taskName, taskDescription, date));
+        disconnect();
+    }
+
+    public ArrayList<Task> query() throws SQLException {
+        connect();
+        ArrayList<Task> taskList = new ArrayList<>();
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM Inbox");
+        while (rs.next()) {
+            taskList.add(new Task(rs.getString("Name"),
+                                  rs.getString("Description"),
+                                  rs.getString("Date")));
+        }
+        return  taskList;
+
+    }
+
     // Singleton
     private DatabaseManager() {}
+
     public static DatabaseManager instance = new DatabaseManager();
+
     public static DatabaseManager getInstance() {
         return instance;
     }
