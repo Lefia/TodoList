520ec992aca443bff3ba849e683de4ca16a2dca0
Lefia <lefia1222@gmail.com>
2023-05-23 01:17:02 +0800

    feat: 新增編輯任務對話框

--- src/main/java/finalproject/todolist/util/DialogManager.java
+++ src/main/java/finalproject/todolist/util/DialogManager.java
@@ -6,7 +6,10 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import finalproject.todolist.component.Task;
 
 public class DialogManager {
     public void addTask() {
@@ -25,28 +28,30 @@ public class DialogManager {
 
         Label nameLabel = new Label("任務名稱");
         root.add(nameLabel, 0, 0);
-        TextField nameTextField = new TextField("New Task");
+        TextField nameTextField = new TextField("新任務");
         root.add(nameTextField, 1, 0);
 
         Label descLabel = new Label("描述");
         root.add(descLabel, 0, 1);
-        TextField descTextField = new TextField("Description");
+        TextField descTextField = new TextField("描述");
         root.add(descTextField, 1, 1);
 
         Label dateLabel = new Label("日期");
         root.add(dateLabel, 0, 2);
         DatePicker datePicker = new DatePicker();
+        datePicker.setValue(LocalDate.now());
         root.add(datePicker, 1, 2);
 
         dialogPane.setContent(root);
 
         ButtonType applyButton = new ButtonType("確認", ButtonBar.ButtonData.APPLY);
         ButtonType closeButton = new ButtonType("取消", ButtonBar.ButtonData.CANCEL_CLOSE);
-        dialogPane.getButtonTypes().addAll(applyButton, closeButton);
+        dialogPane.getButtonTypes().addAll(closeButton, applyButton);
 
         dialogPane.lookupButton(applyButton).addEventHandler(ActionEvent.ACTION, e -> {
             try {
-                DatabaseManager.getInstance().addTask(nameTextField.getText(), descTextField.getText(), datePicker.getValue().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+                Task task = new Task(randomString(10), nameTextField.getText(), descTextField.getText(), datePicker.getValue().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")), false);
+                DatabaseManager.getInstance().addTask(task);
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
@@ -56,6 +61,71 @@ public class DialogManager {
         dialog.setDialogPane(dialogPane);
 
         dialog.show();
+    }
+
+    public void editTask(Task oldTask) {
+        DialogPane dialogPane = new DialogPane();
+        dialogPane.setPrefWidth(250);
+
+        dialogPane.setHeaderText("編輯任務");
+
+        GridPane root = new GridPane();
+        addColumnConstrains(root, 50);
+        addColumnConstrains(root, 50);
+        root.setVgap(3);
+        root.setHgap(2);
+        root.setPadding(new Insets(5));
+        root.prefWidthProperty().bind(dialogPane.widthProperty());
+
+        Label nameLabel = new Label("任務名稱");
+        root.add(nameLabel, 0, 0);
+        TextField nameTextField = new TextField(oldTask.getName());
+        root.add(nameTextField, 1, 0);
+
+        Label descLabel = new Label("描述");
+        root.add(descLabel, 0, 1);
+        TextField descTextField = new TextField(oldTask.getDescription());
+        root.add(descTextField, 1, 1);
+
+        Label dateLabel = new Label("日期");
+        root.add(dateLabel, 0, 2);
+        DatePicker datePicker = new DatePicker();
+        datePicker.setValue(LocalDate.parse(oldTask.getDate(), DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+        root.add(datePicker, 1, 2);
+
+        dialogPane.setContent(root);
+
+        ButtonType applyButton = new ButtonType("確認", ButtonBar.ButtonData.APPLY);
+        ButtonType closeButton = new ButtonType("取消", ButtonBar.ButtonData.CANCEL_CLOSE);
+        dialogPane.getButtonTypes().addAll(closeButton, applyButton);
+
+        dialogPane.lookupButton(applyButton).addEventHandler(ActionEvent.ACTION, e -> {
+            try {
+                Task newTask = new Task(oldTask.getId(), nameTextField.getText(), descTextField.getText(), datePicker.getValue().format(DateTimeFormatter.ofPattern("yyyy/MM/dd")), oldTask.isDone());
+                DatabaseManager.getInstance().editTask(newTask);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        Alert dialog = new Alert(Alert.AlertType.NONE);
+        dialog.setDialogPane(dialogPane);
+
+        dialog.show();
+    }
+
+    private String randomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder stringBuilder = new StringBuilder(length);
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            char randomChar = characters.charAt(index);
+            stringBuilder.append(randomChar);
+        }
+
+        return stringBuilder.toString();
     }
 
     private void addColumnConstrains(GridPane root, double width) {
