caf16223366bd315b5b54ebe728e2287f48b89ed
Lefia <lefia1222@gmail.com>
2023-05-24 17:36:04 +0800

    style: 在 import 間增加空行

--- src/main/java/finalproject/todolist/util/SceneManager.java
+++ src/main/java/finalproject/todolist/util/SceneManager.java
@@ -1,9 +1,11 @@
 package finalproject.todolist.util;
 
 import finalproject.todolist.Main;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.HashMap;
 
